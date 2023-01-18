a7beff0ee47f6b84ec6af097d4d1a8fc5cd507fb
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-18 15:18:11 -0800

    commit tree c5f01766eeadea4f420a566812131c448427a0f7

--- js/reducer.js
+++ js/reducer.js
@@ -4,10 +4,19 @@ function changeState(state, action){
       return {count: state.count + 1}
     default:
       return state;
-  }
-}
+  };
+};
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+let state = {count: 0};
+let action = {type: 'INCREASE_COUNT'};
 
-changeState(state, action)
+function render() {
+  document.body.textContent = state.count;
+};
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+};
+
+render();
